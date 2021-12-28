4525d162ae565b819bb2ded4ffd6a950bab50544
zhangjie <zhangjie@entgene.com>
2021-12-28 19:23:53 +0800

    [update]huskey

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: zhangjie
+ * @Date: 2021-12-28 17:04:16
+ * @LastEditors: zhangjie
+ * @LastEditTime: 2021-12-28 19:15:43
+ * @FilePath: \vue2_cli4_init\src\router\index.js
+ */
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
