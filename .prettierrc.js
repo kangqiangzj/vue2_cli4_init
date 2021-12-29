6eab38f10f5f347c7e1fc3f25e208a0ba8d33afa
zhangjie <zhangjie@entgene.com>
2021-12-29 20:49:33 +0800

    [update]prettier配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,23 @@
+/*
+ * @Author: zhangjie
+ * @Date: 2021-12-29 20:37:44
+ * @LastEditors: zhangjie
+ * @LastEditTime: 2021-12-29 20:49:10
+ * @FilePath: \vue2_cli4_init\.prettierrc.js
+ */
 module.exports = {
     // 最大长度80个字符
     printWidth: 80,
     // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
     singleQuote: true,
     // 行末分号, 默认true
-    semi: true,
+    semi: false,
     // JSX双引号
     jsxSingleQuote: false,
     // 尽可能使用尾随逗号（包括函数参数）,默认none,可选 none|es5|all
     // es5 包括es5中的数组、对象
     // all 包括函数对象等所有可选
-    trailingComma: 'all',
+    trailingComma: 'none',
     // 在对象文字中打印括号之间的空格。 默认true
     bracketSpacing: true,
     // 箭头函数参数括号 默认avoid 可选 avoid| always
@@ -26,9 +33,10 @@ module.exports = {
     // tab缩进大小,默认为2
     tabWidth: 2,
     // 使用tab缩进还是空格，默认false
-    useTabs: true,
+    useTabs: false,
     // vue缩进脚本和样式
     vueIndentScriptAndStyle: false,
     // > 标签放在最后一行的末尾，而不是单独放在下一行 默认false
-    jsxBracketSameLine: false,
-};
+    jsxBracketSameLine: false
+  }
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,16 @@
  * @Author: zhangjie
  * @Date: 2021-12-28 17:04:16
  * @LastEditors: zhangjie
- * @LastEditTime: 2021-12-28 19:47:33
+ * @LastEditTime: 2021-12-29 20:48:03
  * @FilePath: \vue2_cli4_init\src\views\Home.vue
 -->
 
 <template>
-	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
 
-		<HelloWorld msg="Welcome to Your Vue.js App" />
-	</div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
